7e1c1d667672809dd1f67d4ff192c9ec25b9eced
Grzegorz <kalarus.g@gmail.com>
2019-01-28 11:43:41 +0100

    Updates

---
+++ precache-manifest.dbb2b9bd2a6585958b937c770991b9e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48b39b84b796e9c444eb",
+    "url": "/yt-videos/static/js/runtime~main.48b39b84.js"
+  },
+  {
+    "revision": "0e4e68338800f06c9c42",
+    "url": "/yt-videos/static/js/main.0e4e6833.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/yt-videos/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "0e4e68338800f06c9c42",
+    "url": "/yt-videos/static/css/main.42695d29.chunk.css"
+  },
+  {
+    "revision": "e7a8362b1f5c21d5c3c1947b79592c2a",
+    "url": "/yt-videos/index.html"
+  }
+];
